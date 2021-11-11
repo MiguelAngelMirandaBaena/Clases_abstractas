063c8919f42ae24f0efdb1474c3d98ebdfb650f1
MiguelAngelMirandaBaena <the.mikimouseo@gmail.com>
2021-11-11 16:03:03 -0400

    Clases abstractas

---
+++ src/pojos/DiscoSolido.java
@@ -0,0 +1,35 @@
+package pojos;
+
+import interfaces.IReseteable;
+
+import java.util.Date;
+
+public class DiscoSolido extends Tecnologico implements IReseteable{
+    private int capacidad;
+
+    public DiscoSolido(String codigo, String modelo, String paisOrigen, Date fechaFabricacion, Empresa fabricante, int capacidad){
+        super(codigo, modelo, paisOrigen, fechaFabricacion, fabricante);
+
+        this.setCapacidad(capacidad);
+
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+
+    @Override
+    public void estado() {
+        System.out.println("Cantidad de veces que se dio formato al disco: " + 50);
+    }
+
+    @Override
+    public void reset() {
+        System.out.println("Formateando disco solido...");
+    }
+}
